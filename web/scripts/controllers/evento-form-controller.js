b0a13e0a26d2eaeb28fc18325bebbe621deeb44d
Caio Lucena <caioflucena@gmail.com>
2016-07-13 19:42:09 -0300

    visualizacao de evento

--- web/scripts/controllers/evento-form-controller.js
+++ web/scripts/controllers/evento-form-controller.js
@@ -1,4 +1,20 @@
-apolloApp.controller('EventoFormController', ['$scope', function($scope) {
+apolloApp.controller('EventoFormController', ['$scope', '$routeParams', function($scope, $routeParams) {
+    $scope.eventos = [
+                      {id: 1, nome: "Casal 01", dataEvento: "03/08/2017 19:00:00", local: "Local evento 01"},
+                      {id: 2, nome: "Casal 02", dataEvento: "03/08/2017 19:00:00", local: "Local evento 02"},
+                      {id: 3, nome: "Casal 03", dataEvento: "03/08/2017 19:00:00", local: "Local evento 03"},
+                      {id: 4, nome: "Casal 04", dataEvento: "03/08/2017 19:00:00", local: "Local evento 04"},
+                      {id: 5, nome: "Casal 05", dataEvento: "03/08/2017 19:00:00", local: "Local evento 05"},
+                     ];
+    // edicao
+    if ($routeParams.id) {
+        for (evento in $scope.eventos) {
+            if ($scope.eventos[evento].id == $routeParams.id) {
+                $scope.evento = $scope.eventos[evento];
+                break;
+            }
+        }
+    }
     // wrapper
     $( document ).ready(function() {
         $("#dataEvento").datepicker({
@@ -8,6 +24,4 @@ apolloApp.controller('EventoFormController', ['$scope', function($scope) {
             language: "pt-BR"
         });
     });
-    //
-    $scope.evento = {id: 1, nome: "Casal 01", data: "03/08/2017 19:00:00", local: "Local evento 01"};
 }]);
--- web/scripts/controllers/evento-grid-controller.js
+++ web/scripts/controllers/evento-grid-controller.js
@@ -1,11 +1,14 @@
 apolloApp.controller('EventoGridController', ['$scope', function($scope) {
     $scope.eventos = [
-     {id: 1, nome: "Casal 01", data: "03/08/2017 19:00:00", local: "Local evento 01"},
-     {id: 2, nome: "Casal 02", data: "03/08/2017 19:00:00", local: "Local evento 02"},
-     {id: 3, nome: "Casal 03", data: "03/08/2017 19:00:00", local: "Local evento 03"},
-     {id: 4, nome: "Casal 04", data: "03/08/2017 19:00:00", local: "Local evento 04"},
-     {id: 5, nome: "Casal 05", data: "03/08/2017 19:00:00", local: "Local evento 05"},
+     {id: 1, nome: "Casal 01", dataEvento: "03/08/2017 19:00:00", local: "Local evento 01"},
+     {id: 2, nome: "Casal 02", dataEvento: "03/08/2017 19:00:00", local: "Local evento 02"},
+     {id: 3, nome: "Casal 03", dataEvento: "03/08/2017 19:00:00", local: "Local evento 03"},
+     {id: 4, nome: "Casal 04", dataEvento: "03/08/2017 19:00:00", local: "Local evento 04"},
+     {id: 5, nome: "Casal 05", dataEvento: "03/08/2017 19:00:00", local: "Local evento 05"},
     ];
+    $scope.visualizar = function(evento) {
+        console.log(evento);
+    };
     $scope.remove = function(index) {
         $scope.eventos.splice(index, 1);
     }
